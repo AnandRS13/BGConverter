204e7bc5d571de97e5e25b1c0e05efe0466a2632
AnandRS13 <159770088+AnandRS13@users.noreply.github.com>
2024-02-29 17:39:14 +0530

    Updated BGConverter.js

---
+++ BGConverter.js
@@ -0,0 +1,5 @@
+function changeBackgroundColor() {
+    var colorPicker = document.getElementById("colorPicker");
+    var selectedColor = colorPicker.value;
+    document.body.style.backgroundColor = selectedColor;
+}
